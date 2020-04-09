b2ab04950435940b15b8dc1a41805b035b28ef17
daniel <daniellsmarta@gmail.com>
2020-04-09 12:05:24 +0100

    exercise image

--- ss/exercises_exam/exercise_2/5.py
+++ ss/exercises_exam/exercise_2/5.py
@@ -8,6 +8,9 @@ n_B = 100
 p_B = 24 / n_B
 
 confidence = 0.99
+print("Proportion A: {}".format(p_A))
+print("Proportion B: {}".format(p_B))
+
 interval = two_sample_proportion(p_A, n_A, p_B, n_B, confidence)
 print("confidence interval is: [{:.2f} ; {:.2f}] with {:.2f}%".format(interval[0], interval[1],
                                                                       confidence * 100))
